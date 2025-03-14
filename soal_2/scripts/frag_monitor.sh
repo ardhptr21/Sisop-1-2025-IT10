d1a2c9bcac7385709f1296199bb8eb13e45a0ca2
Asarumawari <runnirattancraft@gmail.com>
2025-03-14 21:16:20 +0700

    Update frag_monitor.sh

--- soal_2/scripts/frag_monitor.sh
+++ soal_2/scripts/frag_monitor.sh
@@ -1,24 +1,13 @@
 #!/bin/sh
 
-clear
-ram=$( free -m | awk 'NR==2{printf "%s/%sMB (%.2f%%)\n", $3,$2,$3*100/$2}' )
-aval=$( free -m | awk '/Mem:/ {print $7}' )
-chache=$( free -m | awk '/Mem:/ {print $6}' )
-total=$( free -m | awk '/Mem:/ {print $2}' )
+RAM=$( free -m | awk 'NR==2{printf "%s/%sMB (%.2f%%)\n", $3,$2,$3*100/$2}' )
+AVAILABLE=$( free -m | awk '/Mem:/ {print $7}' )
+CACHE=$( free -m | awk '/Mem:/ {print $6}' )
+TOTAL=$( free -m | awk '/Mem:/ {print $2}' )
 
 echo "‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾"
-echo "==========ARCAEA TERMINAL=========="
-echo ""
-echo "‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾"
-echo "Fragment    | "
+echo "Total     : $TOTAL MB"
+echo "Usage     : $RAM"
+echo "Available : $AVAILABLE MB"
+echo "Cached    : $CACHE MB"
 echo "___________________________________"
-echo ""
-echo "Total       | $total MB"
-echo "Usage       | $ram"
-echo "Available   | $aval MB"
-echo "Cached      | $chache MB"
-echo ""
-echo "‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾"
-echo ""
-read -p "Press Enter to exit" 
-
